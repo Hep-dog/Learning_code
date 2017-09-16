076df076fa62dd4ea22ea01918383a212be6df2f
Hep-dog <shenpx91@gmail.com>
2017-09-16 23:15:52 +0800

    The first commit

--- Autoencoder.py
+++ Autoencoder.py
@@ -68,7 +68,7 @@ with tf.Session() as sess:
             _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
 
         if epoch % display_step == 0:
-            print("Epoch: ", '%%04d' % (epoch+1), "cost= ", "{:.9f}".format(c))
+            print("Epoch: ", '%04d' % (epoch+1), "cost= ", "{:.9f}".format(c))
 
     print("Optimization Finished")
 
